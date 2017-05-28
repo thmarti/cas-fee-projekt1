ce9d66947428f5dd91b682629baf5a3c1803c842
Thomas Marti <tmarti@hsr.ch>
2017-05-28 13:23:14 +0200

    - handlebars & pouchdb - responsive

---
+++ src/loadTemplate.js
@@ -0,0 +1,62 @@
+const LoadTemplate = function(element, template, data){
+    // Check if parenet element is defined as string or object.
+    if(typeof element === 'string'){
+        this.el = document.getElementById(element);
+    } else {
+        this.el = element;
+    }
+
+    // Store template name and data.
+    this.tempName = template;
+    this.data = data || null;
+
+    // You can change this to path of your template folder.
+    this.folderPath = 'templates/';
+};
+
+LoadTemplate.prototype.create = function(callback){
+    let req = new XMLHttpRequest();
+    let that = this;
+
+    // Define parameters for request.
+    req.open('get', this.folderPath + this.tempName + '.handlebars', true);
+
+    // Wait for request to complete.
+    req.onreadystatechange = function(){
+        if (req.readyState === 4 && req.status === 200){
+            //Compile HB template, add data (if defined) and place in parent element.
+            let compiled = Handlebars.compile(req.response);
+            that.el.innerHTML = compiled(that.data);
+
+            // Execute callback function
+            callback();
+        }
+    };
+
+    // Send request.
+    req.send();
+};
+
+/*
+LoadTemplate.prototype.createAndWait = function(callback){
+    let req = new XMLHttpRequest();
+    let that = this;
+
+    // Define parameters for request.
+    req.open('get', this.folderPath + this.tempName + '.handlebars', true);
+
+    // Wait for request to complete.
+    req.onreadystatechange = function(){
+        if (req.readyState === 4 && req.status === 200){
+            //Compile HB template, but wait..
+            let compiled = Handlebars.compile(req.response);
+
+            // Execute callback function and parse variables.
+            callback(compiled, that.el);
+        }
+    };
+
+    // Send request.
+    req.send();
+};
+*/
