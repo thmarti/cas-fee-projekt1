8c6c4fc543ccadd3c700122780bd526218d4a018
Thomas Marti <tmarti@hsr.ch>
2017-06-28 13:37:40 +0200

    stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,14 @@
 import './handelbars.helpers.js';
 import Routes from './routes.js';
+import NotesOverviewController from './notes-overview.controller.js';
+import NoteDetailController from './note-detail.controller.js';
 
-let routes = new Routes();
+let notesOverviewController = new NotesOverviewController();
+let noteDetailController = new NoteDetailController();
 
+let routes = new Routes(notesOverviewController, noteDetailController);
+notesOverviewController.setRoutes(routes);
+noteDetailController.setRoutes(routes);
+
+notesOverviewController.show();
 
